189728d0b5cb58170538d80426e5ee74f81e766b
Raquitico <contato.java@hotmail.com>
2023-05-08 17:50:09 -0300

    fix: removing-code

--- src/app/components/user/users-create/users-create.component.ts
+++ src/app/components/user/users-create/users-create.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HelperService } from 'src/app/shared/services/helper.service';
-import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-users-create',
@@ -9,33 +7,5 @@ import { v4 as uuidv4 } from 'uuid';
 })
 export class UsersCreateComponent {
 
-  name: string = "";
-  profession: string = "";
-  pet: string = "";
-  email: string = "";
-  phone: string = "";
-
-  constructor(private helperService: HelperService) { }
-
-  registerUser() {
-    const data = {
-      name: this.name,
-      profession: this.profession,
-      pet: this.pet,
-      phone: this.phone,
-      email: this.email,
-      id: uuidv4(),
-    }
-    this.helperService.createUser(data).then(() => {
-      this.clearToSendAgain();
-    })
-  }
-
-  clearToSendAgain() {
-    this.name = "";
-    this.profession = "";
-    this.pet = "";
-    this.phone = "",
-    this.email = ""
-  }
+  constructor() { }
 }
--- src/app/components/user/users-edit/users-edit.component.ts
+++ src/app/components/user/users-edit/users-edit.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { HelperService } from 'src/app/shared/services/helper.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/shared/models/user.model';
-import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-users-edit',
@@ -13,39 +12,24 @@ export class UsersEditComponent {
 
   user!: User;
   userId: string = '';
-  form!: FormGroup;
 
   constructor(
-    private helperService: HelperService, 
-    private fb: FormBuilder,
+    private helperService: HelperService,
     private route: ActivatedRoute,
     private router: Router
   ) { }
 
   async ngOnInit(): Promise<void> {
-    this.form = this.fb.group({
-      name: [null],
-      id: [null],
-      profession: [null],
-      pet: [null],
-      email: [null],
-      phone: [null],
-    });
     this.route.params.subscribe(params => {
       this.userId = params['id'];
     });
     try {
       this.user = await this.helperService.getOneUser(this.userId);
-      if(this.user.id == undefined){
+      if (this.user.id == undefined) {
         this.router.navigate(['/'])
       }
-      this.form.patchValue(this.user)
     } catch (error) {
       console.log(error);
     }
-  }
-
-  editValues() {
-    this.helperService.editUser(this.userId, this.form.value)
   }
 }
